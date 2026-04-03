bbe744c475556abb8b0d279fcd3271cd33329194
agustinportoo <tanitoporto@gmail.com>
2026-04-03 13:35:32 -0300

    Update price_calculator.py

--- price_calculator.py
+++ price_calculator.py
@@ -1,4 +1,4 @@
-# ---- Funciones provistas (NO modificar) ----
+---- Funciones provistas (NO modificar) ----
 
 def apply_discount(price, discount_pct):
     """Dado un precio y un porcentaje de descuento, retorna el precio con el descuento aplicado."""
@@ -21,7 +21,12 @@ def final_price(price, quantity, discount_pct, tax_pct):
       3. Aplicar el impuesto al resultado usando apply_tax.
       4. Retornar el resultado redondeado a 2 decimales usando round().
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    subtotal=(price * quantity)
+    subd=apply_discount(subtotal, discount_pct)
+    subt=apply_tax(subd, tax_pct)
+    total=round(subt,2)
+    return total
+
 
 def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     """
@@ -31,4 +36,9 @@ def best_deal(price_a, qty_a, disc_a, price_b, qty_b, disc_b, tax_pct):
     Si son iguales, retorna "A".
     Debe USAR la función final_price para resolver el ejercicio.
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if final_price(price_a, qty_a, disc_a, tax_pct) < final_price(price_b, qty_b, disc_b, tax_pct):
+        return "A"
+    elif final_price(price_a, qty_a, disc_a, tax_pct) > final_price(price_b, qty_b, disc_b, tax_pct):
+        return "B"
+    else:
+        return "A"
